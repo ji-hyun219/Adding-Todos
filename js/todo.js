b1c69cc2c9cb66ff91611a662f8315b99871278b
ji-hyun219 <91349474+ji-hyun219@users.noreply.github.com>
2021-10-11 01:36:08 +0900

    made TodoList
    function Adding Todolist is paintTodo.
    look at Todo.js.

---
+++ js/todo.js
@@ -0,0 +1,31 @@
+﻿const todoForm = document.querySelector('#todo_form');
+const todoList = document.querySelector('#todo-list');
+const todoInput = document.querySelector('#todo_form input');
+
+
+function deleteTodo(event) {
+    
+}
+
+
+function paintTodo(newlist) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const button = document.createElement('button');
+    todoList.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(button);  // <li><span></span><button></button></li>
+    span.innerText = newlist;
+    button.innerText = '❌';
+    button.addEventListener('click', deleteTodo);
+}
+
+
+function onSubmitTodoForm(event) {
+    event.preventDefault();
+    const newList = todoInput.value;
+    todoInput.value = "";
+    paintTodo(newList);
+}
+
+todoForm.addEventListener('submit', onSubmitTodoForm);
